72871ce162db64af6ede57320acfa63c237b5b60
Mohd Omama <mohd.omama@gmail.com>
2018-03-06 07:46:44 +0530

    W4 working condition

---
+++ W4/quickSort.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class Sort{
+private:
+	int *array;
+	int size;
+
+public:
+	void input(){
+		cout << "Enter the size of array:" <<endl;
+		cin >> size;
+		array = (int *) malloc (sizeof(int)*size);
+		cout << "Enter the array to be sorted:" <<endl;
+		for (int i = 0; i < size; ++i){
+			cin >> array[i];
+		}
+	}
+
+	void print(){
+		for (int i = 0; i < size; ++i){
+			cout << array[i] << " ";
+		}
+		cout << endl;
+	}
+
+	void quick_sort(int begin, int end){
+		if(begin < end){
+			int pivot_index = hoare_partition(begin, end);
+			quick_sort(begin, pivot_index - 1);
+			quick_sort(pivot_index + 1, end);
+		}
+	}
+
+	int hoare_partition(int begin, int end){
+		int pivot = array[end];
+		int i = begin - 1;
+		int j = end + 1;
+		while (true){
+			do{
+				j = j - 1;
+			}while(array[j] > pivot);
+
+			do{
+				i = i + 1;
+			}while(array[i] < pivot);
+			if(i < j){
+				array[i] = array[j] + array[i];
+				array[j] = array[i] - array[j];
+				array[i] = array[i] - array[j];
+			}
+			else{
+				return j;
+			}
+		}
+	}
+
+	int get_size(){
+		return size;
+	}
+};
+
+int main(int argc, char const *argv[]){
+
+	Sort array;
+	array.input(); 
+	array.quick_sort(0, array.get_size() - 1);
+	array.print();
+	return 0;
+}
