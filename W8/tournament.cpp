81463606242e860c9c89a99e0b2128ac008340ec
Mohd Omama <mohd.omama@gmail.com>
2018-04-17 15:59:36 +0530

    W8: OnSpot complete

--- W8/tournament.cpp
+++ W8/tournament.cpp
@@ -7,13 +7,19 @@ using namespace std;
 class Pool {
 private:
 	int *array;
+	int *team_name;
 	int size;
 public:
 	Pool (int size) {
 		array = new int[size];
+		team_name = new int[size];
 		this->size = size;
+
 		for (int i = 0; i < size; i++) {
 			array[i] = 0;
+		}
+		for (int i = 0; i < size; i++) {
+			team_name[i] = i;
 		}
 	}
 
@@ -46,33 +52,74 @@ public:
 		}
 	}
 
-	int max () {
+	void victory () {
 		int max = array[0];
+		int team = 0;
 		for (int i = 0; i < size; i++) {
-			if (array[i] > max)
+			if (array[i] > max){
 				max = array[i];
+				team = i;
+			}
 		}
-		return max;
+		cout << "Team" << team << " won with points " << max << endl;
 
 	}
 	void print () {
-		cout << "Points Tally:" <<endl;
+		cout << "\nPoints Tally:" <<endl;
 		for (int i = 0; i < size; i++) {
-			cout << "Team" << i << " : " << array[i] << "  ";
+			cout  << array[i] << "  ";
+		}
+		cout << endl;
+
+		cout << "Team Names: " <<endl;
+
+		for (int j = 0; j < size; j++) {
+			//cout << j << endl;
+			cout << team_name[j] << "  ";
 		}
 		cout << endl;
 	}
 	void sort () {
-		std::sort( array, array + size);
-		std::reverse( array, array + size);
+		for (int j = size -1; j >= 1; j--){
+			for (int i = 0; i < j; i++){
+				if (array[i] < array[i+1]) {
+					swap(array[i], array[i+1]);
+					swap(team_name[i], team_name[i+1]);
+				}
+			}
+		}
+	}
+	void quarter_finals () {
+		cout << "Quater Finals" << endl;
+
+		assign (&array[0], &array[1], team_name[0], team_name[1]);
+		assign (&array[2], &array[3], team_name[2], team_name[3]);
+
+	}
+
+	void finals () {
+		cout << "Finals" << endl;
+		
+		assign (&array[0], &array[1], team_name[0], team_name[1]);
+
 	}
 };
 
 int main (int argc, char const *argv[]) {
 	srand (time(NULL));
-	Pool pool1(5);
+	Pool pool1(10);
 	pool1.play();
-	//pool1.sort();
+	pool1.victory();
+	pool1.print();
+	pool1.sort();
+	pool1.print();
+
+	pool1.quarter_finals();
+	pool1.sort();
+	pool1.print();
+
+	pool1.finals();
+	pool1.sort();
 	pool1.print();
 	return 0;
 }
