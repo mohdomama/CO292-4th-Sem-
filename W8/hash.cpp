9073bb5956ae1e8accd3441056a179e44821ff6a
Mohd Omama <mohd.omama@gmail.com>
2018-04-17 12:08:56 +0530

    W8: Add interface

--- W8/hash.cpp
+++ W8/hash.cpp
@@ -75,7 +75,7 @@ public:
 	void print (){
 		struct node* temp = Head;
 		while (temp!=NULL) {
-			cout << temp->value << " -- "; 
+			cout << temp->value << " --> "; 
 			temp = temp->next;
 		}
 		cout << "||" <<endl;
@@ -100,33 +100,63 @@ public:
 
 	void remove (int key) {
 		int hash = hash_function (key);
+
 		buckets[hash].remove(key);
 	}
 
 	void print_buckets() {
 		for (int i = 0; i < hash_size; i++) {
+			cout << "Bucket " << i << " :  ";
 			buckets[i].print();
 		}
 	}
 
 	int hash_function (int key) {
 		int hash = key%hash_size;
-		return hash;
+		return hash < 0 ? hash + hash_size : hash;
 	}
 
 };
 
 int main (int argc, char const *argv[]) {
 
+	int option, key;
 	HashMap hash(5);
 
-	hash.insert(1);
-	hash.insert(2);
-	hash.insert(3);
-	hash.insert(4);
-	hash.insert(5);
-	hash.insert(6);
-	hash.print_buckets();
+	while (1) {
+		cout << "Enter the option:" << endl;
+		cout << "0) Exit" << endl;
+		cout << "1) Add key" << endl;
+		cout << "2) Remove key" << endl;
+		cout << "3) Print Hash Table" << endl;
+
+		cin >> option;
+
+		switch (option) {
+			case 0: {
+				return 0;
+				break;
+			}
+			case 1: {
+				cout << "Enter key to be inserted:" <<endl;
+				cin >> key;
+				hash.insert(key);
+				break;
+			}
+			case 2: {
+				cout << "Enter value to be inserted:" <<endl;
+				cin >> key;
+				hash.remove(key);
+				break;
+			}
+			case 3: {
+				hash.print_buckets();
+				break;
+			}
+		}
+
+		cout << "\n" << endl;
+	}
 
 	return 0;
 }
